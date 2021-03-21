bf8238356a42176957d2f533a3cbb7674784e904
kitajii <kazu.rmcf@gmail.com>
2021-03-21 07:22:44 +0000

    Laravel10

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create','Admin\newsController@add');
+    Route::get('news/create','Admin\NewsController@add');
     Route::get('profile/create','Admin\profileController@edit'); //課題4(PHP/Laravel 09 Routingについて理解する)
 });
 
