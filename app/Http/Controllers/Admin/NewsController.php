0d4a9b1faf604f9fc4128e360dac2b559f147d34
kitajii <kazu.rmcf@gmail.com>
2021-03-23 09:44:23 +0000

    laravel13

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -8,7 +8,11 @@ use App\Http\Controllers\Controller;
 class NewsController extends Controller
 {
     // 以下を追記
-    public function add() {
+    public function add() { //フォーム送信していない状態の表示？
         return view('admin.news.create');
+    }
+    public function create(Request $request) { //useで使用する事を宣言しているRequestというクラス
+      // admin/news/createにリダイレクトする
+      return redirect('admin/news/create');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create','Admin\NewsController@add')->middleware('auth'); //news/createにアクセスしたら、認証ユーザーのみアクセスを許可し、認証されていなかったらlogin画面へ。Http/Karnelで登録済み
-    Route::get('profile/create','Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware'=>'auth'], function() {
+    Route::get('news/create','Admin\NewsController@add');
+    Route::post('news/create','Admin\NewsController@create');
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::post('profile/create','Admin\ProfileController@create');
+    Route::get('profile/edit','Admin\ProfileController@edit');
+    Route::post('profile/edit','Admin\ProfileController@update');
 });
 
 Route::get('XXX','AAAController@bbb'); //課題3(PHP/Laravel 09 Routingについて理解する)
